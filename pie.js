458906918e52c4c14747d13c94f6c98a05bf5318
Kirill Khalevin <kkh@level.travel>
2021-10-19 22:32:50 +0300

    JS skeleton

--- pie.js
+++ pie.js
@@ -1,14 +1,138 @@
+
+function isNode(node) { return node instanceof Element; }
+
+function findNode(id_or_class, whichClass=0)
+{
+    if (typeof id_or_class === "string") {
+        node = document.getElementById(id_or_class);
+        if (isNode(node)) { return node; }
+        nodes_by_class = document.getElementsByClassName(id_or_class);
+        if (nodes_by_class.length > 0)
+        {
+            return nodes_by_class.length > whichClass ? nodes_by_class[whichClass] : nodes_by_class[0];
+        }
+    }
+
+    return undefined;
+}
+
+function createNode(tag, parentNode=document.body, options={})
+{
+    node = document.createElement(tag);
+    for (var option in options){
+        var digArr = option.split('.');
+        var obj = node;
+        for (var digLevel = 0; digLevel < digArr.length-1; digLevel++){ obj = obj[digArr[digLevel]]; }
+        obj[digArr.pop()] = options[option];
+    }
+    if (!isNode(parentNode)){ parentNode = findNode(parentNode); }
+    if (!isNode(parentNode)){ parentNode = document.body; }
+    parentNode.appendChild(node);
+    return node;
+}
+
 //Code collected from https://code.tutsplus.com/ru/tutorials/how-to-draw-a-pie-chart-and-doughnut-chart-using-javascript-and-html5-canvas--cms-27197
 
 class Piechart{
     constructor(options)
     {
-        this.pie_colors_default = ["#F66D44","#FEAE65","#E6F69D","#AADEA7","#64C2A6","#2D87BB","#FFF1C9","#F7B7A3","#EA5F89","#9B3192","#57167E","#2B0B3F"];
-        this.options = options;
-        this.canvas = options.canvas;
-        this.ctx = this.canvas.getContext("2d");
+        this.pie_colors_default = ["#F68D64","#FEAE65","#E6F69D","#AADEA7","#64C2A6","#6DE7BB","#FFF1C9","#F7B7A3","#EA5F89","#9B3192","#57167E","#2B0B3F"];
         this.colors = options.colors.concat(this.pie_colors_default);
         this.shaded_colors = this.colors.map(function(color) { return shadeColor(color, -20); } );
+        this.removedColors = [];
+        this.nextColor = 0;
+
+        this.chartID = this.randomID();
+        this.options = options;
+        this.canvasSize = options.canvasSize;
+        this.parentNode = options.parentNode;
+        this.tableRowsID = {};
+        this.generateHTMLSkelet();
+        this.addRows();
+    }
+
+    randomID(){
+        return Math.floor(Math.random()*16777215).toString(16);
+    }
+
+    getNextColor(){
+        var currentColor = this.colors[this.nextColor % this.colors.length];
+        this.removedColors.length > 0 ? currentColor = this.removedColors.pop() : this.nextColor += 1;
+        return currentColor;
+    }
+
+    //https://24ways.org/2010/calculating-color-contrast
+    getContrastYIQ(hexcolor){
+        var r = parseInt(hexcolor.substr(1,2),16);
+        var g = parseInt(hexcolor.substr(3,2),16);
+        var b = parseInt(hexcolor.substr(5,2),16);
+        var yiq = ((r*299)+(g*587)+(b*114))/1000;
+        return (yiq >= 128) ? '#616A6B' : 'white';
+    }
+
+    //types = empty, string, number, remove
+    addRow(values, tdTypes)
+    {
+        var currentColor = this.getNextColor();
+        var rowID = this.randomID();
+        var tr = createNode("tr", this.table, {"id": "row-".concat(rowID)});
+        var valuePos = 0;
+        tdTypes.forEach((tdType) => {
+            var td = createNode("td", tr);
+            if (tdType == 'empty') {
+                td.style.background = currentColor;
+                td.innerHTML = "&nbsp;&nbsp;&nbsp;&nbsp;";
+            }
+            else if (tdType == 'remove')
+            {
+                var span = createNode("span", td);
+                var btn = createNode("button", span, {
+                    "type": "button",
+                    "className": "btn btn-danger btn-rounded btn-sm my-0",
+                    "innerText": "X",
+                    "id": "row-remove-".concat(rowID)
+                })
+            } else {
+                //td.style.backgroundColor = currentColor;
+                //td.style.opacity = "0.5";
+                if (['string', "number"].includes(tdType)){
+                    var nodeOptions = {
+                        "placeholder": "Item",
+                        "value": values[valuePos]
+                    }
+                    if (tdType == "number") {
+                        nodeOptions["type"] = "number";
+                        nodeOptions["step"] = "1000";
+                        nodeOptions["placeholder"] = "0";
+                    }
+                    var input = createNode("input", td, nodeOptions)
+                }
+            }
+            valuePos++;
+        });
+    }
+
+    addRows()
+    {
+        for (var categ in this.options.data){
+            var values = ["", categ, this.options.data[categ]]
+            var tdTypes = ["empty", "string", "number", "remove"]
+            this.addRow(values, tdTypes)
+        }
+    }
+
+    generateHTMLSkelet()
+    {
+        this.leftPart = createNode("div", this.parentNode, {"className": "col-md-7 col-lg-5"});
+        this.table = createNode("table", this.leftPart,
+                    {"className": "table table-responsive-md text-center", "id": "table-".concat(this.chartID)});
+
+
+        this.rightPart = createNode("div", this.parentNode, {"className": "col-md-7 col-lg-5"});
+        this.canvas = createNode("canvas", this.rightPart, {"id": "canvas-".concat(this.chartID)});
+        this.canvas.width = this.canvasSize;
+        this.canvas.height = this.canvasSize;
+        this.ctx = this.canvas.getContext("2d");
     }
 
     drawPieSlice(ctx,centerX, centerY, radius, startAngle, endAngle, color ){
@@ -44,13 +168,12 @@ class Piechart{
         document.getElementsByTagName('head')[0].appendChild(link);
     };
 
-    draw(){
+    drawSectors()
+    {
         var total_value = 0;
         var shift = 10;
         var coef_padding = 0.03
 
-        var canvas_width = this.canvas.width;
-        var canvas_height = this.canvas.height;
         for (var categ in this.options.data){
             var val = this.options.data[categ];
             total_value += val;
@@ -67,9 +190,9 @@ class Piechart{
                 var current_colors = shift == 0 ? this.colors : this.shaded_colors;
                 this.drawPieSlice(
                     this.ctx,
-                    canvas_width/2-shift,
-                    canvas_height/2+shift,
-                    Math.min(canvas_width/2-canvas_width*coef_padding,canvas_height/2-canvas_height*coef_padding),
+                    this.canvas.width/2-shift,
+                    this.canvas.height/2+shift,
+                    Math.min(this.canvas.width/2-this.canvas.width*coef_padding,this.canvas.height/2-this.canvas.height*coef_padding),
                     start_angle,
                     start_angle+slice_angle,
                     current_colors[color_index%this.colors.length]
@@ -80,21 +203,31 @@ class Piechart{
             }
             shift -= 1
         }
+    }
 
-        start_angle = 0;
+    drawPercentLabels()
+    {
+        var total_value = 0;
+
+        for (var categ in this.options.data){
+            var val = this.options.data[categ];
+            total_value += val;
+        }
+
+        var start_angle = 0;
         for (categ in this.options.data){
-            val = this.options.data[categ];
-            slice_angle = 2 * Math.PI * val / total_value;
-            var pieRadius = Math.min(canvas_width/2,canvas_height/2);
-            var labelX = canvas_width/2 + (pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
+            var val = this.options.data[categ];
+            var slice_angle = 2 * Math.PI * val / total_value;
+            var pieRadius = Math.min(this.canvas.width/2,this.canvas.height/2);
+            var labelX = this.canvas.width/2 + (pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
             labelX -= labelX*0.05;
-            var labelY = canvas_height/2 + (pieRadius / 2) * Math.sin(start_angle + slice_angle/2);
+            var labelY = this.canvas.height/2 + (pieRadius / 2) * Math.sin(start_angle + slice_angle/2);
             //labelY += labelY*0.05;
 
             if (this.options.doughnutHoleSize){
                 var offset = (pieRadius * this.options.doughnutHoleSize ) / 2;
-                labelX = canvas_width/2 + (offset + pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
-                labelY = canvas_height/2 + (offset + pieRadius / 2) * Math.sin(start_angle + slice_angle/2);
+                labelX = this.canvas.width/2 + (offset + pieRadius / 2) * Math.cos(start_angle + slice_angle/2);
+                labelY = this.canvas.height/2 + (offset + pieRadius / 2) * Math.sin(start_angle + slice_angle/2);
 
             }
             var percents = Math.round(100 * val / total_value)
@@ -106,20 +239,12 @@ class Piechart{
             }
             start_angle += slice_angle;
         }
+    }
 
-        if (this.options.legend){
-            color_index = 0;
-            var legendHTML = "";
-            var input_element = "";
-            for (categ in this.options.data){
-                legendHTML += "<div><span style='display:inline-block;width:20px;background-color:"+this.colors[color_index++]+";'>&nbsp;</span> "+categ;
-                legendHTML += `<input type="number" step="1000" name="pie-value-${categ}">`;
-                legendHTML += `%<input type="checkbox" name="pie-value-${categ}-in-percents"><Br></div>`;
-            }
-            this.options.legend.innerHTML = legendHTML;
-        }
-
-        this.setFaviconTag()
+    draw(){
+        this.drawSectors();
+        this.drawPercentLabels();
+        this.setFaviconTag();
     }
 }
 
@@ -169,18 +294,18 @@ var myVinyls = {
     "13" : 2,
 }
 
-var myCanvas = document.getElementById("myCanvas");
-myCanvas.width = 460;
-myCanvas.height = 460;
-var ctx = myCanvas.getContext("2d");
-var myLegend = document.getElementById("myLegend");
+const editor = document.getElementById("pie-editor");
+const canvasSize = 460;
+
+
 
 var myDougnutChart = new Piechart(
     {
-        canvas:myCanvas,
+        parentNode:editor,
+        canvasSize:canvasSize,
         data:myVinyls,
         colors:["#fde23e"],
-        legend:myLegend
+        //table:tableValues
     }
 );
 
